7139416317abd4d7a8e42f90134f06fb7a8ec2bf
kryptonedu <86527068+kryptonedu@users.noreply.github.com>
2021-10-17 19:13:49 +0530

    Create primefinder.py

---
+++ Python/primefinder.py
@@ -0,0 +1,16 @@
+numb = int (input ("number: ")
+
+if numb > 1:
+          for i in range (2, numb):
+
+                             if (numb% i) == 0:
+
+                                            print ("It is not a prime number")
+
+                            else:
+
+                                            print ("It is a prime number")
+
+else:
+
+              print ("It is not a prime number")
